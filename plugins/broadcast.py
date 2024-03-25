2d52c1d9ace3c935b22f97217ab23db4aa5e2bd3
SpaciousAbhi <74132323+SpaciousAbhi@users.noreply.github.com>
2024-03-25 15:48:44 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,66 +1,73 @@
 
 from pyrogram import Client, filters
 import datetime
-import asyncio
+import time
 from database.users_chats_db import db
 from info import ADMINS
 from utils import broadcast_messages
-
+import asyncio
+        
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
-async def broadcast_to_users(bot, message):
+# https://t.me/GetTGLink/4178
+async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
     sts = await message.reply_text(
         text='Broadcasting your messages...'
     )
-    start_time = datetime.datetime.now()
+    start_time = time.time()
     total_users = await db.total_users_count()
-    success = 0
+    done = 0
     blocked = 0
     deleted = 0
-    failed = 0
+    failed =0
 
-    async def send_message(user):
-        nonlocal success, blocked, deleted, failed
+    success = 0
+    async for user in users:
         pti, sh = await broadcast_messages(int(user['id']), b_msg)
         if pti:
             success += 1
         elif pti == False:
             if sh == "Blocked":
-                blocked += 1
+                blocked+=1
             elif sh == "Deleted":
                 deleted += 1
             elif sh == "Error":
                 failed += 1
-
-    tasks = [send_message(user) for user in users]
-    await asyncio.gather(*tasks)
-
-    time_taken = datetime.datetime.now() - start_time
-    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken.total_seconds()} seconds.\n\nTotal Users {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}\nFailed: {failed}")
+        done += 1
+        await asyncio.sleep(2)
+        if not done % 20:
+            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
 
 @Client.on_message(filters.command("grp_broadcast") & filters.user(ADMINS) & filters.reply)
-async def broadcast_to_groups(bot, message):
+async def grp_brodcst(bot, message):
     chats = await db.get_all_chats()
     b_msg = message.reply_to_message
     sts = await message.reply_text(
         text='Broadcasting your messages...'
     )
-    start_time = datetime.datetime.now()
+    start_time = time.time()
     total_chats = await db.total_chat_count()
-    success = 0
-    failed = 0
+    done = 0
+    failed =0
 
-    async def send_message(chat):
-        nonlocal success, failed
+    success = 0
+    async for chat in chats:
         pti, sh = await broadcast_messages(int(chat['id']), b_msg)
         if pti:
             success += 1
         elif pti == False:
-            failed += 1
-
-    tasks = [send_message(chat) for chat in chats]
-    await asyncio.gather(*tasks)
-
-    time_taken = datetime.datetime.now() - start_time
-    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken.total_seconds()} seconds.\n\nTotal Chats {total_chats}\nSuccess: {success}\nFailed: {failed}")
+            if sh == "Blocked":
+                blocked+=1
+            elif sh == "Deleted":
+                deleted += 1
+            elif sh == "Error":
+                failed += 1
+        done += 1
+        await asyncio.sleep(2)
+        if not done % 20:
+            await sts.edit(f"Broadcast in progress:\n\nTotal Chats {total_chats}\nCompleted: {done} / {total_chats}\nSuccess: {success}\nFailed: {failed}")    
+    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Chats {total_chats}\nCompleted: {done} / {total_chats}\nSuccess: {success}\nFailed: {failed}")
